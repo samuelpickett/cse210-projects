acd489ac9564269b5e1ab4169f32f531d6858df0
samuelpickett <pic21007@byui.edu>
2025-09-22 09:20:00 -0600

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string play = "y";
+        while (play == "y")
+        {
+            Random numGenerator = new Random();
+            int number = numGenerator.Next(0, 100);
+            Console.Write("What is your guess for the magic number? ");
+            int guess = int.Parse(Console.ReadLine());
+            int numGuesses = 0;
+            while (guess != number)
+            {
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                    numGuesses++;
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                    numGuesses++;
+                }
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"You got it in {numGuesses} guesses. ");
+            Console.Write("Would you like to play again? y/n");
+            play = Console.ReadLine();
+        }
     }
 }
