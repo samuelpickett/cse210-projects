b4be40db91df4c32eabb9a89a77e7123c68dffbd
samuelpickett <pic21007@byui.edu>
2025-09-24 09:43:56 -0600

    Finished Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int squaredNumber = SquareNumber(PromptUserNumber());
+        int birthYear;
+        PromptUserBirthYear(out birthYear);
+
+        DisplayResult(name, squaredNumber, birthYear);
     }
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int favorite = int.Parse(Console.ReadLine());
+            return favorite;
+        }
+
+        static void PromptUserBirthYear(out int birthYear)
+        {
+            Console.Write("Please enter the year you were born: ");
+            birthYear = int.Parse(Console.ReadLine());
+        }
+
+        static int SquareNumber(int number)
+        {
+            return number * number;
+        }
+
+        static void DisplayResult(string userName, int squaredNumber, int year)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {squaredNumber}.");
+            Console.WriteLine($"{userName}, you will turn {2025 - year} this year");
+        }
+    
 }
