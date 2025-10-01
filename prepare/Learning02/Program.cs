0826199ded6584c9235abd49f01302e43b547ec9
samuelpickett <pic21007@byui.edu>
2025-10-01 09:19:28 -0600

    Finished W03 Prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 1990;
+        job1._endYear = 2025;
+
+        Job job2 = new();
+        job2._company = "Apple";
+        job2._jobTitle = "Marketing Director";
+        job2._startYear = 2090;
+        job2._endYear = 3000;
+
+        Resume resume = new();
+        resume._name = "John Smith";
+        resume._jobs = [job1, job2];
+        resume.Display();
     }
 }
